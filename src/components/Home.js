9aa8d70f495d84468fcd48a063bc689986783d59
Christian Rey Villablanca <chriztiann.villablanca@gmail.com>
2022-03-02 00:43:37 +0800

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import fish from "../assets/fish.png";
 
 const Main = styled.div`
   height: 100%;
   width: 100%;
+  max-width: 400vh;
+  min-width: 200vh;
 `;
 
 const NavBar = styled.div`
-  background-color: red;
+  // background-color: red;
   height: 80px;
   display: flex;
   flex-direction: row;
@@ -17,9 +20,12 @@ const NavBar = styled.div`
 `;
 
 const Container = styled.div`
-  background-color: aliceblue;
   height: auto;
   width: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 const Body = styled.div`
@@ -33,26 +39,54 @@ const Body = styled.div`
 const RightNav = styled.div`
   background-color: blue;
   height: auto;
-  width: 25%;
+  width: 20%;
 `;
 
 const LeftNav = styled.div`
   background-color: brown;
   height: auto;
-  width: 25%;
+  width: 20%;
 `;
 
 const Feeds = styled.div`
   background-color: beige;
   height: 100px;
-  width: 50%;
+  width: 60%;
+  
 `;
+
+const FishbookIcon = styled.img`
+  width: 30px;
+  height: 30px;
+`;
+
+const SearchBar = styled.div`
+  height: 30px;
+  width: 50px;
+  
+`;
+
+const Search = styled.input`
+  background-color: aliceblue;
+  height: auto;
+  width: auto;
+  margin-left: 10px;
+  border-radius: 5px;
+  padding: 5px;
+`;
+
+
 
 function Home(props) {
   return (
     <Main>
       <NavBar>
-        <Container>test</Container>
+        <Container>
+          <FishbookIcon src={fish}></FishbookIcon>
+          <SearchBar>
+            <Search />
+          </SearchBar>
+        </Container>
         <Container>asdwqe</Container>
         <Container>asdwqe</Container>
       </NavBar>
