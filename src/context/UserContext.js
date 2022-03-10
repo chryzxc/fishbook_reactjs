71f824b37b24d32b341f11da64d8d0c2f2b22bdf
Christian Rey Villablanca <chriztiann.villablanca@gmail.com>
2022-03-11 00:53:58 +0800

    commit

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,22 +1,47 @@
-import React, { createContext, Component } from "react";
+import React, { createContext, useState } from "react";
+import db from "../others/firebase";
+import {
+  ref,
+  set,
+  push,
+  getDatabase,
+  child,
+  get,
+  equalTo,
+  orderByChild,
+} from "firebase/database";
 
 export const UserContext = createContext();
 
-class UserContextProvider extends Component {
-  state = {
-      id: "",
-      firstname:"" ,
-      middlename:"",
-      lastname: "",
-      email: "",
+const UserContextProvider = (props) => {
+  const [user, setUser] = useState({
+    id: "",
+    firstname: "CHristian",
+    middlename: "",
+    lastname: "",
+    email: "",
+  });
+
+  const fetchData = (userId) => {
+    const dbRef = ref(db);
+    get(child(dbRef, "users/" + userId))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+         
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
-  render() {
-    return (
-      <UserContext.Provider value={{ ...this.state }}>
-        {this.props.children}
-      </UserContext.Provider>
-    );
-  }
-}
+
+  fetchData();
+
+  return (
+    <UserContext.Provider value={{ user }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContextProvider;
