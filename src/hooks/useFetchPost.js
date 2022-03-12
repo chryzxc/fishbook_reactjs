88420ec5d5df983b0bcc62e73d30f7ac2c6c3e0d
Christian Rey Villablanca <chriztiann.villablanca@gmail.com>
2022-03-12 21:51:08 +0800

    commit

--- src/hooks/useFetchPost.js
+++ src/hooks/useFetchPost.js
@@ -1,36 +1,62 @@
 import { useState, useEffect } from "react";
-import { onValue } from "firebase/database";
+import { onValue, get, child } from "firebase/database";
 
 const useFetchPost = (dbRef) => {
   const [fetchedData, setFetchedData] = useState(null);
   const [isStillFetching, setIsStillFetching] = useState(true);
- 
+
   useEffect(() => {
-    onValue(dbRef, (snapshot) => {
-      const fetchedData = [];
-      snapshot.forEach((data) => {
-        const dataVal = data.val();
+    get(dbRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const fetchedData = [];
+          console.log("to be fetched : " + snapshot);
+          snapshot.forEach((data) => {
+            const dataVal = data.val();
 
-        fetchedData.push({
-          user_id: dataVal.user_id,
-          caption: dataVal.caption,
-          contents: dataVal.contents,
-          date_posted: dataVal.date_posted,
-          liked_users: dataVal.liked_users,
-          likes: dataVal.likes,
-        
-        });
+            fetchedData.push({
+              post_id: data.key,
+              user_id: dataVal.user_id,
+              caption: dataVal.caption,
+              contents: dataVal.contents,
+              date_posted: dataVal.date_posted,
+            });
+          });
+
+          setIsStillFetching(false);
+          setFetchedData(fetchedData);
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
       });
-
-      setIsStillFetching(false);
-     
-      setFetchedData(fetchedData);
-    });
-   
   }, []);
- 
-  return { fetchedData , isStillFetching};
 
+  // useEffect(() => {
+  //   onValue(dbRef, (snapshot) => {
+  //     const fetchedData = [];
+  //     snapshot.forEach((data) => {
+  //       const dataVal = data.val();
+
+  //       fetchedData.push({
+  //         user_id: dataVal.user_id,
+  //         caption: dataVal.caption,
+  //         contents: dataVal.contents,
+  //         date_posted: dataVal.date_posted,
+  //         liked_users: dataVal.liked_users,
+  //         likes: dataVal.likes,
+  //       });
+  //     });
+
+  //     setIsStillFetching(false);
+
+  //     setFetchedData(fetchedData);
+  //   });
+  // }, []);
+
+  return { fetchedData, isStillFetching };
 };
 
 export default useFetchPost;
