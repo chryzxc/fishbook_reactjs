d0769b55944f705b9a1eda3542e608477c7905ba
Christian Rey Villablanca <chriztiann.villablanca@gmail.com>
2022-03-29 21:21:27 +0800

    COMMIT

--- src/hooks/useGetUserData.js
+++ src/hooks/useGetUserData.js
@@ -2,21 +2,54 @@ import React, { useEffect, useState } from "react";
 import { ref, child, onValue } from "firebase/database";
 import { db } from "../others/firebase";
 
-const useGetUserData = async (profileId) => {
+const useGetUserData = (myId,profileId) => {
   const dbRef = ref(db, "users/" + profileId);
   const [data, setData] = useState(null);
-  await useEffect(() => {
+
+  
+  
+ 
+   useEffect(() => {
     onValue(dbRef, (snapshot) => {
       if (snapshot.exists()) {
-        // console.log("user :" + snapshot.val().firstname);
-        setData(snapshot);
+        let profileData = null;
+        let requestSent = false;
+
+     
+          const friend_requests = snapshot?.val().friend_requests;
+          const friend_requests_list = [];
+          if (friend_requests) {
+            Object.keys(friend_requests).map((key) => {
+              friend_requests_list.push(key);
+            });
+            if (friend_requests_list.includes(myId)) {
+              requestSent = true;
+            } else {
+              requestSent= false;
+            }
+          }
+        
+      
+        profileData = {
+          firstname: snapshot?.val().firstname,
+          lastname:snapshot?.val().lastname,
+          email: snapshot?.val().email,
+          date_registered: snapshot?.val().date_registered,
+          friends: snapshot?.val().friends,
+          request_sent: requestSent,
+        };
+
+
+         console.log("user :" + profileData);
+        setData(profileData);
       } else {
         setData();
       }
     });
+ 
   }, []);
-
   return data;
+  
 };
 
 export default useGetUserData;
